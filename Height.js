63533b2a819ab63d72eda1f7b1719cfcbb384261
Jeeves <83597947+jeeves148@users.noreply.github.com>
2022-03-18 12:14:53 +0000

    Prototype 3

--- Height.js
+++ Height.js
@@ -4,9 +4,13 @@ function calculateHeight()
     let bs = document.getElementById("bodyTypeID").value;
     console.log(a)
     console.log(bs)
-    
+
+    if(isNaN(a) || a == "")
+    {document.getElementById("outputID").innerHTML = "Height must be a number , you silly sausage"}
+    else
+    {
     text = bs * a
     console.log(text);
     document.getElementById("outputID").innerHTML = text + "cm";
-
+    }
 }
